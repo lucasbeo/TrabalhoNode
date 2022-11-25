3ab075a3619c6f78c6110f3ff2d48b67a6d3676e
mellodrama <joaopedromello344@gmail.com>
2022-11-25 10:44:46 -0300

    Placar jogo do Brasil

--- Aula1.js
+++ Aula1.js
@@ -3,6 +3,7 @@ var dia = require('./biblioteca');
 http.createServer(function(req, res){
     res.writeHead(200, {'Content-Type':'text/html'})
     res.write('<h2> Brasil 2 x 0 Servia </h2><br>');
+    res.write('<h2> Brasil 1 x 0 Suiça </h2)')
     res.write("<p style= color:green>" + Date().substring(16, 24) + "<br>");
     res.write(dia.diaDaSemana() + "</p><br>")
     res.end('Bom dia!');
