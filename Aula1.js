3668141b95e5370740c1a3343b155d40dc7455f4
mellodrama <joaopedromello344@gmail.com>
2022-11-25 10:16:52 -0300

    Versão 2 - placar atualizado e data

--- Aula1.js
+++ Aula1.js
@@ -1,6 +1,8 @@
 var http = require('http');
 http.createServer(function(req, res){
-    res.write('Brasil 3 x 1 Servia \n');
+    res.writeHead(200, {'Content-Type':'text/html'})
+    res.write('<h2> Brasil 2 x 0 Servia </h2><br>');
+    res.write(Date().substring(16, 24) + "<br><br>");
     res.end('Bom dia!');
 
 }).listen(8080);
